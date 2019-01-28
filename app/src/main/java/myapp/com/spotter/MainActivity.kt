1f8f36f5bd8db902e9ce677bd05849c9fa87a54c
Jeremy Razafinirina <jeremyraza@gmail.com>
2019-01-28 12:45:47 +0100

    feat(card_activity): -> started card view xml

--- app/src/main/java/myapp/com/spotter/MainActivity.kt
+++ app/src/main/java/myapp/com/spotter/MainActivity.kt
@@ -2,14 +2,11 @@ package myapp.com.spotter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
     }
 }
