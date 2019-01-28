425ce652cbadb1f934c42dceeefdffcc75cb9102
Jeremy Razafinirina <jeremyraza@gmail.com>
2019-01-28 11:02:37 +0100

    feat(bottom_navigation) -> started the bottom navigation

--- app/src/main/java/myapp/com/spotter/ListActivity.kt
+++ app/src/main/java/myapp/com/spotter/ListActivity.kt
@@ -3,10 +3,10 @@ package myapp.com.spotter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class Main2Activity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_list)
     }
 }
