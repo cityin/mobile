e25658f5e50dd16f12cd5f6dae5671345979cd34
edouard <edouard.gachet@hetic.net>
2019-01-28 10:32:51 +0100

    refresh fils

--- app/src/main/java/myapp/com/spotter/MainActivity.kt
+++ app/src/main/java/myapp/com/spotter/MainActivity.kt
@@ -17,25 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-        //val emailEditText: EditText = findViewById(R.id.emailEditText)
-        //val passwordEditText: EditText = findViewById(R.id.passwordEditText)
-        //val buttonLogin: Button = findViewById(R.id.buttonLogin)
-
-        //val listener = View.OnClickListener(){
-            //Toast.makeText(this, "hello world", Toast.LENGTH_LONG).show()
-        //var valueTextEmail = emailEditText.text.toString()
-        //var valueTextPassword = passwordEditText.text.toString()
-
-        //if(valueTextEmail.contains("@") && valueTextEmail.contains(".com") && valueTextPassword.length > 5){
-        // Toast.makeText(this, "C'est bon !", Toast.LENGTH_LONG).show()
-        //val intent = Intent(this, Main2Activity::class.java)
-        //startActivity(intent)
-        //}else{
-        //Toast.makeText(this, "Il y'a une erreur !", Toast.LENGTH_LONG).show()
-        //}
-        //}
-        //buttonLogin.setOnClickListener(listener)
+        
     }
 }
