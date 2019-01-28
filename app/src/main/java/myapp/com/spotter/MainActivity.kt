79496289a9152041cbf27ff20d617b80063b3c2a
edouard <edouard.gachet@hetic.net>
2019-01-28 09:33:04 +0100

    chaud

--- app/src/main/java/myapp/com/spotter/MainActivity.kt
+++ app/src/main/java/myapp/com/spotter/MainActivity.kt
@@ -6,13 +6,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         //val emailEditText: EditText = findViewById(R.id.emailEditText)
         //val passwordEditText: EditText = findViewById(R.id.passwordEditText)
@@ -24,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         //var valueTextPassword = passwordEditText.text.toString()
 
         //if(valueTextEmail.contains("@") && valueTextEmail.contains(".com") && valueTextPassword.length > 5){
-                //Toast.makeText(this, "C'est bon !", Toast.LENGTH_LONG).show()
+        // Toast.makeText(this, "C'est bon !", Toast.LENGTH_LONG).show()
         //val intent = Intent(this, Main2Activity::class.java)
         //startActivity(intent)
         //}else{
