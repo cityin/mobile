02a9cfa298f2e0aa5c2399a7cab9d00cfc2c4aa1
Jeremy Razafinirina <jeremyraza@gmail.com>
2019-01-30 14:34:17 +0100

    feat(main_fragment): -> setup card view layout

--- app/src/main/java/myapp/com/spotter/adapter/MainAdapter.kt
+++ app/src/main/java/myapp/com/spotter/adapter/MainAdapter.kt
@@ -3,7 +3,9 @@ package myapp.com.spotter.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.card_layout.view.*
 import myapp.com.spotter.R
 import myapp.com.spotter.network.response.Activity
@@ -11,14 +13,13 @@ import myapp.com.spotter.network.response.ResultActivities
 
 class MainAdapter(val activities: List<Activity>): RecyclerView.Adapter<CustomViewHolder>() {
     
-    // return numbers of items
+    // Return numbers of items
     override fun getItemCount(): Int {
         return activities.count()
     }
 
+    // How to create a view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        // How to create a view
-
         val layoutInflater = LayoutInflater.from(parent?.context)
         val cellFlow = layoutInflater.inflate(R.layout.card_layout, parent, false)
 
@@ -26,16 +27,18 @@ class MainAdapter(val activities: List<Activity>): RecyclerView.Adapter<CustomVi
 
     }
 
+    // How to bind the data into the view
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val activity = activities.get(position)
 
         holder.itemView.cardTitle.text = activity.name
         holder.itemView.cardDate.text = "12 janvier 2018"
+
+        // Use Picasso to set the image url into the cardView
+        val cardPicture = holder.itemView.cardImageView
+
+        Picasso.get().load("https://picsum.photos/500/300/?random").into(cardPicture)
     }
-
-
 }
 
-class CustomViewHolder(v: View): RecyclerView.ViewHolder(v) {
-
-}
+class CustomViewHolder(v: View): RecyclerView.ViewHolder(v) {}
