b7ee24a6c5d824cb738618d9b620757c8eb5bbf5
unknown <edouard.gachet@hetic.net>
2019-06-04 13:12:09 +0200

    add recyclerView Spotter in whislistActivities

--- app/src/main/java/myapp/com/cityin/fragment/wish_list_fragment/WishListFragmentTravelBandActivities.kt
+++ app/src/main/java/myapp/com/cityin/fragment/wish_list_fragment/WishListFragmentTravelBandActivities.kt
@@ -9,14 +9,22 @@ import androidx.core.view.ViewCompat
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_wish_list_fragment_folder_activities.*
+import kotlinx.android.synthetic.main.fragment_wishlist.*
 import kotlinx.android.synthetic.main.item_activity.*
 import myapp.com.cityin.R
 import myapp.com.cityin.adapter.FolderActivitiesAdapter
+import myapp.com.cityin.adapter.FolderAdapter
+import myapp.com.cityin.adapter.SpotterItemsAdapter
 import myapp.com.cityin.network.ActivitiesService
+import myapp.com.cityin.network.FoldersService
+import myapp.com.cityin.network.response.Spotters
 
 class WishListFragmentTravelBandActivities : androidx.fragment.app.Fragment() {
     val args: WishListFragmentTravelBandActivitiesArgs by navArgs()
     lateinit var folderId: String
+    lateinit var folderName: String
+    lateinit var folderDesription: String
+    lateinit var folderSpotter: Array<Spotters>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_wish_list_fragment_folder_activities, container, false)
@@ -26,12 +34,24 @@ class WishListFragmentTravelBandActivities : androidx.fragment.app.Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         folderId = args.folderId
-        activitiesRecyclerView.layoutManager = LinearLayoutManager(context)
+        folderName = args.folderName
+        val folderActivity = args.folderActivity.toInt()
+        folderDesription = args.folderDescription
+        folderSpotter = args.folderSpotter
 
+        TitleTravelBand.text = folderName.toString()
+        descriptionTravelBand.text = folderDesription.toString()
+        folderNumberOfActivitiesTextView.text = "${folderActivity.toString()} activités enregistrées"
+
+        var recyclerView = ItemsSpotterRecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = SpotterItemsAdapter(folderSpotter)
+
+        activitiesRecyclerView.layoutManager = LinearLayoutManager(context)
 
         ActivitiesService.getActivitiesByTravelBandId(folderId, {
             activities -> activities.size
-            activitiesRecyclerView.adapter = FolderActivitiesAdapter(activities)
+            activitiesRecyclerView?.adapter = FolderActivitiesAdapter(activities)
 
             ViewCompat.setNestedScrollingEnabled(activitiesRecyclerView, false);
         }, {
